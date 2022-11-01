33d91a473140077dc258f6f3e4173e90f7cafc9e
awan <me.awan.es@gmail.com>
2022-11-02 05:44:04 +0700

    change variable array to arrayNumber

--- main.cpp
+++ main.cpp
@@ -1,33 +1,33 @@
 #include <iostream>
 
 using namespace std;
-void BubbleSort(int ArrayNumber[100], int n);
-void printArray(int ArrayNumber[100], int n);
+void BubbleSort(int arrayNumber[100], int n);
+void printArray(int arrayNumber[100], int n);
 
 int main()
 {
-    int ArrayNumber[10] = {9,2,7,4,8};
-    BubbleSort(ArrayNumber, 5);
+    int arrayNumber[10] = {9,2,7,4,8};
+    BubbleSort(arrayNumber, 5);
     return 0;
 }
 
-void BubbleSort(int ArrayNumber[100], int n){
-    printArray(ArrayNumber, n);
+void BubbleSort(int arrayNumber[100], int n){
+    printArray(arrayNumber, n);
     for(int i=0; i < n - 1; i++){
         for(int j=i+1; j < n; j++){
-            if(ArrayNumber[j] < ArrayNumber[i]){
-                int temp = ArrayNumber[i];
-                ArrayNumber[i] = ArrayNumber[j];
-                ArrayNumber[j] = temp;
+            if(arrayNumber[j] < arrayNumber[i]){
+                int temp = arrayNumber[i];
+                arrayNumber[i] = arrayNumber[j];
+                arrayNumber[j] = temp;
             }
         }
     }
-    printArray(ArrayNumber, n);
+    printArray(arrayNumber, n);
 }
 
-void printArray(int ArrayNumber[100], int n){
+void printArray(int arrayNumber[100], int n){
     for(int i=0; i < n; i++){
-        cout << ArrayNumber[i] << " ";
+        cout << arrayNumber[i] << " ";
     }
     cout << endl;
 }
