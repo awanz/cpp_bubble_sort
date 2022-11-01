0beb9cf7fae9955c04591be1692e722b175d73d6
awan <me.awan.es@gmail.com>
2022-11-02 05:43:15 +0700

    change variable array to ArrayNumber

--- main.cpp
+++ main.cpp
@@ -1,33 +1,33 @@
 #include <iostream>
 
 using namespace std;
-void BubbleSort(int A[100], int n);
-void printArray(int A[100], int n);
+void BubbleSort(int ArrayNumber[100], int n);
+void printArray(int ArrayNumber[100], int n);
 
 int main()
 {
-    int A[10] = {9,2,7,4,8};
-    BubbleSort(A, 5);
+    int ArrayNumber[10] = {9,2,7,4,8};
+    BubbleSort(ArrayNumber, 5);
     return 0;
 }
 
-void BubbleSort(int A[100], int n){
-    printArray(A, n);
+void BubbleSort(int ArrayNumber[100], int n){
+    printArray(ArrayNumber, n);
     for(int i=0; i < n - 1; i++){
         for(int j=i+1; j < n; j++){
-            if(A[j] < A[i]){
-                int temp = A[i];
-                A[i] = A[j];
-                A[j] = temp;
+            if(ArrayNumber[j] < ArrayNumber[i]){
+                int temp = ArrayNumber[i];
+                ArrayNumber[i] = ArrayNumber[j];
+                ArrayNumber[j] = temp;
             }
         }
     }
-    printArray(A, n);
+    printArray(ArrayNumber, n);
 }
 
-void printArray(int A[100], int n){
+void printArray(int ArrayNumber[100], int n){
     for(int i=0; i < n; i++){
-        cout << A[i] << " ";
+        cout << ArrayNumber[i] << " ";
     }
     cout << endl;
 }
