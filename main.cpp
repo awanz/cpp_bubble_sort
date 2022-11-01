71fe0e66558f92591e07f4839ab75e214dd01145
awan <me.awan.es@gmail.com>
2022-11-02 05:40:46 +0700

    fix logix bubble sort

--- main.cpp
+++ main.cpp
@@ -8,24 +8,26 @@ int main()
 {
     int A[10] = {9,2,7,4,8};
     BubbleSort(A, 5);
-    printArray(A, 5);
     return 0;
 }
 
 void BubbleSort(int A[100], int n){
+    printArray(A, n);
     for(int i=0; i < n - 1; i++){
-        for(int j=n; j > i; j--){
-            if(A[j] < A[j-1]){
-                int temp = A[j];
-                A[j] = A[j-1];
-                A[j-1] = temp;
+        for(int j=i+1; j < n; j++){
+            if(A[j] < A[i]){
+                int temp = A[i];
+                A[i] = A[j];
+                A[j] = temp;
             }
         }
     }
+    printArray(A, n);
 }
 
 void printArray(int A[100], int n){
     for(int i=0; i < n; i++){
         cout << A[i] << " ";
     }
+    cout << endl;
 }
